df42b0da3f2f00efd8427406670e095b4dc02e3b
Aleks Gomeniuk <agsfinks@gmail.com>
2025-10-05 15:13:58 +0100

    fix: fix unlimited retries

--- src/app.tsx
+++ src/app.tsx
@@ -2,10 +2,9 @@ import './index.css';
 import 'swiper/swiper-bundle.css';
 
 import { CssBaseline, ThemeProvider } from '@mui/material';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 
-import Loading from './components/loading/Loading.tsx';
 import { tokenCache } from './ecommerce/clientBuilder.ts';
 import { useAppDispatch, useAppSelector } from './redux/hooks.ts';
 import { getThemeName } from './redux/selectors.ts';
@@ -19,23 +18,27 @@ import { ELocalStorage } from './services/interfaces/createCart.interface.ts';
 import { darkTheme, lightTheme } from './theme/theme.ts';
 
 export const App = () => {
-  const { isError, refetch: refetchCart } = useGetActiveCartQuery({});
+  const { refetch: refetchCart } = useGetActiveCartQuery({});
+  const [noServer, setNoServer] = useState('');
 
   const theme = useAppSelector(getThemeName);
   const dispatch = useAppDispatch();
   const [createCart] = useCreateCartMutation();
 
   useEffect(() => {
-    const createCartQuery = async () => {
-      await createCart({
-        currency: 'BYN',
-        useAuthClient: Boolean(localStorage.getItem(ELocalStorage.isAuth)),
-      }).unwrap();
-      await refetchCart().unwrap();
-    };
-
-    if (isError) createCartQuery();
-  }, [createCart, isError, refetchCart]);
+    try {
+      const createCartQuery = async () => {
+        await createCart({
+          currency: 'BYN',
+          useAuthClient: Boolean(localStorage.getItem(ELocalStorage.isAuth)),
+        }).unwrap();
+        await refetchCart().unwrap();
+      };
+      createCartQuery();
+    } catch (error: unknown) {
+      if (error instanceof Error) setNoServer(error.message);
+    }
+  }, [createCart, refetchCart]);
 
   useEffect(() => {
     const refreshToken = tokenCache.get().refreshToken;
@@ -48,7 +51,8 @@ export const App = () => {
   return (
     <ThemeProvider theme={currentTheme}>
       <CssBaseline />
-      {!isError ? <Router /> : <Loading />}
+      <div>{noServer}</div>
+      <Router />
       <ToastContainer position="bottom-right" />
     </ThemeProvider>
   );
